9792b694c15990317715709c10fbefb1b02c8707
GitLabSJ <aeeou12@gmail.com>
2023-07-01 13:56:48 +0900

    Design: Upload Item

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -42,13 +42,17 @@ export default function Enter() {
           </div>
         </div>
         <form className="flex flex-col">
-          <label className="mt-8 text-sm font-medium text-gray-500">
+          <label
+            htmlFor="input"
+            className="mt-8 text-sm font-medium text-gray-500"
+          >
             {method === "email" ? "Email address" : null}
             {method === "phone" ? "Phone number" : null}
           </label>
           <div className="mt-1">
             {method === "email" ? (
               <input
+                id="input"
                 type="email"
                 className="w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
                 required
@@ -60,6 +64,7 @@ export default function Enter() {
                   +82
                 </span>
                 <input
+                  id="input"
                   type="number"
                   className="rounder-l-none w-full appearance-none rounded-r-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
                   required
