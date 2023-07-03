552613bd12b5c9ed9ae6adf0a267da0d8101e168
GitLabSJ <aeeou12@gmail.com>
2023-07-03 14:06:27 +0900

    Feat: Validation

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,19 +1,42 @@
-import { useForm } from "react-hook-form";
-
+import { FieldErrors, useForm } from "react-hook-form";
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
 export default function Forms() {
-  const { register } = useForm();
+  const { register, handleSubmit } = useForm<LoginForm>();
+
+  const onValid = (data: LoginForm) => {
+    console.log("im valid bby");
+    console.log(data);
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
-        {...register("name")}
+        {...register("username", {
+          required: "username is required",
+          minLength: {
+            message: "The username should be longer than 5 chars.",
+            value: 5,
+          },
+        })}
         type="text"
         placeholder="Username"
         required
       />
-      <input {...register("email")} type="email" placeholder="Email" required />
       <input
-        {...register("password")}
+        {...register("email", { required: "Email is required" })}
+        type="email"
+        placeholder="Email"
+        required
+      />
+      <input
+        {...register("password", { required: "Password is required" })}
         type="password"
         placeholder="Password"
         required
