d1e664fa52f6c6c17d5e2d02ef422a4081b7d1cf
GitLabSJ <aeeou12@gmail.com>
2023-07-04 17:44:43 +0900

    Refactor: Path

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@/libs/server/clients";
-import withHandler from "../../../libs/server/withHandler";
+import withHandler from "@/libs/server/withHandler";
+
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
   return res.json(req.body);
