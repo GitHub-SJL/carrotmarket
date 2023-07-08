4be1477348da408d0116aafa6bbf9d7cb22a3d41
GitLabSJ <aeeou12@gmail.com>
2023-07-08 11:07:56 +0900

    Feat: create User & Token

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,8 +3,27 @@ import client from "@/libs/server/clients";
 import withHandler from "@/libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  return res.json(req.body);
+  const { phone, email } = req.body;
+  const user = phone ? { phone: +phone } : { email };
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
+    },
+  });
+  console.log(token);
+  return res.status(200).end();
 }
 
 export default withHandler("POST", handler);
