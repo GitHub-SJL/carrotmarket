4651aa3abf814bde329af430c9d8bd24fa3882e1
GitLabSJ <aeeou12@gmail.com>
2023-07-08 14:04:44 +0900

    Feat: Create an SMS login authentication service using twilio

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import twilio from "twilio";
 import client from "@/libs/server/clients";
-import withHandler from "@/libs/server/withHandler";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { phone, email } = req.body;
-  const user = phone ? { phone: +phone } : { email };
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user)
+    return res.status(400).json({
+      ok: false,
+    });
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
     data: {
@@ -22,8 +32,19 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       },
     },
   });
-  console.log(token);
-  return res.status(200).end();
+
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      body: `Your login token is ${payload}`,
+      from: "+18147873034",
+      to: process.env.MY_PHONE!,
+    });
+    console.log(message);
+  }
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);
