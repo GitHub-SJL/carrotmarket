deabc336e00cb5472f17ee4467b0445f95ac7d00
GitLabSJ <aeeou12@gmail.com>
2023-07-11 19:41:40 +0900

    Feat: index of Community

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -10,25 +10,50 @@ async function handler(
   const { question } = req.body;
   const { user } = req.session;
 
-  const post = await client.post.create({
-    data: {
-      question,
-      user: {
-        connect: {
-          id: user?.id,
+  if (req.method === "POST") {
+    const post = await client.post.create({
+      data: {
+        question,
+        user: {
+          connect: {
+            id: user?.id,
+          },
         },
       },
-    },
-  });
-  res.json({
-    ok: true,
-    post,
-  });
+    });
+    res.json({
+      ok: true,
+      post,
+    });
+  }
+  if (req.method === "GET") {
+    const posts = await client.post.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            avatar: true,
+          },
+        },
+        _count: {
+          select: {
+            Wondering: true,
+            Answer: true,
+          },
+        },
+      },
+    });
+    res.json({
+      ok: true,
+      posts,
+    });
+  }
 }
 
 export default withApiSession(
   withHandler({
-    methods: ["POST"],
+    methods: ["GET", "POST"],
     handler,
   })
 );
--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -2,27 +2,43 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import FloatingButton from "@/components/floating-button";
 import Layout from "@/components/layout";
+import useSWR from "swr";
+import { Post, User } from "@prisma/client";
+
+interface PostWithUser extends Post {
+  user: User;
+  _count: {
+    Wondering: number;
+    Answer: number;
+  };
+}
+
+interface PostsResponse {
+  ok: boolean;
+  posts: PostWithUser[];
+}
 
 const Community: NextPage = () => {
+  const { data } = useSWR<PostsResponse>(`/api/posts`);
   return (
     <Layout hasTabBar title="동네생활">
       <div className="space-y-4 divide-y-[2px]">
-        {[1, 2, 3, 4, 5, 6].map((_, i) => (
+        {data?.posts?.map((post) => (
           <Link
-            key={i}
+            key={post.id}
             className="flex cursor-pointer flex-col items-start pt-4"
-            href={`/community/${i}`}
+            href={`/community/${post.id}`}
           >
             <span className="ml-4 flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800">
               동네질문
             </span>
             <div className="mt-2 px-4 text-gray-700">
-              <span className="font-medium text-orange-500">Q.</span> What is
-              the best mandu restaurant?
+              <span className="font-medium text-orange-500">Q.</span>{" "}
+              {post.question}
             </div>
             <div className="mt-5 flex w-full items-center justify-between px-4 text-xs font-medium text-gray-500">
-              <span>니꼬</span>
-              <span>18시간 전</span>
+              <span>{post.user.name}</span>
+              <span>{post.createdAt}</span>
             </div>
             <div className="mt-3 flex w-full space-x-5 border-t px-4 py-2.5   text-gray-700">
               <span className="flex items-center space-x-2 text-sm">
@@ -40,7 +56,7 @@ const Community: NextPage = () => {
                     d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                   ></path>
                 </svg>
-                <span>궁금해요 1</span>
+                <span>궁금해요 {post._count.Wondering}</span>
               </span>
               <span className="flex items-center space-x-2 text-sm">
                 <svg
@@ -57,7 +73,7 @@ const Community: NextPage = () => {
                     d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                   ></path>
                 </svg>
-                <span>답변 1</span>
+                <span>답변 {post._count.Answer}</span>
               </span>
             </div>
           </Link>
