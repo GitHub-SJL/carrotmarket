9cf74914d142f2890ffe874d3bf4b20c247aebc3
GitLabSJ <aeeou12@gmail.com>
2023-07-12 14:24:02 +0900

    Feat: edit profile

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -2,16 +2,66 @@ import type { NextPage } from "next";
 import Button from "@/components/button";
 import Input from "@/components/input";
 import Layout from "@/components/layout";
+import useUser from "@/libs/client/useUser";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+import useMutation from "@/libs/client/useMutation";
+interface EditProfileForm {
+  email?: string;
+  phone?: string;
+  name?: string;
+  formErrors?: string;
+}
+
+interface EditProfileResponse {
+  ok: boolean;
+  error?: string;
+}
 
 const EditProfile: NextPage = () => {
+  const { user } = useUser();
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<EditProfileForm>();
+
+  useEffect(() => {
+    if (user?.name) setValue("name", user.name);
+    if (user?.email) setValue("email", user.email);
+    if (user?.phone) setValue("phone", user.phone);
+  }, [user, setValue]);
+
+  const [editProfile, { data, loading }] =
+    useMutation<EditProfileResponse>("/api/users/me");
+
+  useEffect(() => {
+    if (data && !data.ok && data.error) {
+      setError("formErrors", { message: data.error });
+    }
+  }, [data, setError]);
+
+  const onValid = ({ email, phone, name }: EditProfileForm) => {
+    if (loading) return;
+    if (email === "" && phone === "" && name === "") {
+      setError("formErrors", {
+        message: "Email OR Phone number are required. You need to choose one.",
+      });
+    }
+
+    editProfile({ email, phone, name });
+  };
+
   return (
     <Layout canGoBack title="Edit Profile">
-      <form className="py-10 px-4 space-y-4">
+      <form onSubmit={handleSubmit(onValid)} className="space-y-4 px-4 py-10">
         <div className="flex items-center space-x-3">
-          <div className="w-14 h-14 rounded-full bg-slate-500" />
+          <div className="h-14 w-14 rounded-full bg-slate-500" />
           <label
             htmlFor="picture"
-            className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
+            className="cursor-pointer rounded-md border border-gray-300 px-3 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
           >
             Change
             <input
@@ -22,15 +72,34 @@ const EditProfile: NextPage = () => {
             />
           </label>
         </div>
-        <Input required label="Email address" name="email" type="email" />
         <Input
-          required
+          register={register("name")}
+          required={false}
+          label="Name"
+          name="name"
+          type="text"
+        />
+        <Input
+          register={register("email")}
+          required={false}
+          label="Email address"
+          name="email"
+          type="email"
+        />
+        <Input
+          required={false}
+          register={register("phone")}
           label="Phone number"
           name="phone"
-          type="number"
+          type="text"
           kind="phone"
         />
-        <Button text="Update profile" />
+        {errors.formErrors ? (
+          <span className="my-2 block text-center font-medium text-red-500">
+            {errors.formErrors.message}
+          </span>
+        ) : null}
+        <Button text={loading ? "Loading..." : "Update profile"} />
       </form>
     </Layout>
   );
